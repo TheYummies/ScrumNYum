704004a5a8ba1a0afe56fe25be20ed0019aea7d0
Anthony Lo <anthonylo@Anthonys-MacBook-Pro.local>
2022-08-12 20:38:43 -0700

    initial config complete

--- server/server.js
+++ server/server.js
@@ -1,3 +1,15 @@
-function hello() {
-  console.log('hellloooooo');
-}
+const path = require('path');
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+// serve static assets
+app.use('/src', express.static(path.resolve(__dirname, '../src')));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+});
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App.js';
+
+// to bundle styles
+import styles from './app.css';
+
+render(<App />, document.getElementById('root'));
