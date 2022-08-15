e0cbccba19cad67711d78916d44076385fe6b7ee
janeyou94 <janeyou94@gmail.com>
2022-08-15 18:05:08 -0400

    updated sql postgres file

--- scrumnyum_postgres_create.sql
+++ scrumnyum_postgres_create.sql
@@ -11,39 +11,39 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 CREATE TABLE public.user (
-	"UserID" varchar NOT NULL,
-	"Password" varchar NOT NULL,
-	CONSTRAINT "user_pk" PRIMARY KEY ("UserID")
+	"id" varchar NOT NULL,
+	"password" varchar NOT NULL,
+	CONSTRAINT "user_pk" PRIMARY KEY ("id")
 ) WITH (
   -- what is OIDS???
   OIDS=FALSE
 );
 
 CREATE TABLE public.workspace (
-	"WorkspaceID" varchar NOT NULL,
-	"Workspace_PW" varchar NOT NULL,
-	CONSTRAINT "workspace_pk" PRIMARY KEY ("WorkspaceID")
+	"id" varchar NOT NULL,
+	"workspace_password" varchar NOT NULL,
+	CONSTRAINT "workspace_pk" PRIMARY KEY ("id")
 ) WITH (
   -- what is OIDS???
   OIDS=FALSE
 );
 
 CREATE TABLE public.stickies (
-	"StickieID" serial NOT NULL,
-	"Title" varchar NOT NULL,
-	"Description" varchar NOT NULL,
-	"Snack_ID" varchar NOT NULL,
-	"Assigned_ID" varchar NOT NULL,
-	"Workspace_ID" varchar NOT NULL,
-	CONSTRAINT "stickies_pk" PRIMARY KEY ("StickieID")
+	"stickie_id" serial NOT NULL,
+	"id" varchar NOT NULL,
+	"description" varchar NOT NULL,
+	"snack_id" varchar NOT NULL,
+	"assigned_id" varchar NOT NULL,
+	"workspace_id" varchar NOT NULL,
+	CONSTRAINT "stickies_pk" PRIMARY KEY ("stickie_id")
 ) WITH (
   -- what is OIDS???
   OIDS=FALSE
 );
 
 CREATE TABLE public.snacks (
-	"SnackID" varchar NOT NULL,
-	CONSTRAINT "snacks_pk" PRIMARY KEY ("SnackID")
+	"id" varchar NOT NULL,
+	CONSTRAINT "snacks_pk" PRIMARY KEY ("id")
 ) WITH (
   -- what is OIDS???
   OIDS=FALSE
@@ -59,9 +59,9 @@ CREATE TABLE public.snacks (
 --   OIDS=FALSE
 -- )
 
-ALTER TABLE public.stickies ADD CONSTRAINT "stickies_fk0" FOREIGN KEY ("Snack_ID") REFERENCES  public.snacks("SnackID");
-ALTER TABLE public.stickies ADD CONSTRAINT "stickies_fk1" FOREIGN KEY ("Assigned_ID") REFERENCES  public.user("UserID");
-ALTER TABLE public.stickies ADD CONSTRAINT "stickies_fk2" FOREIGN KEY ("Workspace_ID") REFERENCES  public.workspace("WorkspaceID");
+ALTER TABLE public.stickies ADD CONSTRAINT "stickies_fk0" FOREIGN KEY ("snack_id") REFERENCES  public.snacks("id");
+ALTER TABLE public.stickies ADD CONSTRAINT "stickies_fk1" FOREIGN KEY ("assigned_id") REFERENCES  public.user("id");
+ALTER TABLE public.stickies ADD CONSTRAINT "stickies_fk2" FOREIGN KEY ("workspace_id") REFERENCES  public.workspace("id");
 
 -- ALTER TABLE public.userWorkspaceJoin ADD CONSTRAINT "userworkspacejoin_fk0" FOREIGN KEY ("User_UserID") REFERENCES  public.user("UserID");
 -- ALTER TABLE public.userWorkspaceJoin ADD CONSTRAINT "userworkspacejoin_fk1" FOREIGN KEY ("Workspace_WorkspaceID") REFERENCES  public.workspace("WorkspaceID");
