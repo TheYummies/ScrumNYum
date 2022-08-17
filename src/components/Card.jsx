661b69440211c77c2a35d0f2c847c2820bfbb754
LBLuc <lshsu01@gmail.com>
2022-08-17 15:34:00 -0700

    navbar done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -22,7 +22,15 @@ function Card(props) {
     >
       {/* generic label that don't know children ahead of time */}
       {props.children}
-      {/* displays the map prop drilled from scrum?? (card) */}
+      <div className="dropdown">
+        <button type='button' class='btn btn-warning dropdown-toggle data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"'>setting</button>
+        <div class="dropdown-menu">
+          <a class='dropdown-item' href='#'>View details</a>
+          <a class='dropdown-item' href='#'>Edit</a>
+          <a class='dropdown-item' href='#'>delete</a>
+        </div>
+        {/* displays the map prop drilled from scrum?? (card) */}
+      </div>
     </div>
   );
 }
